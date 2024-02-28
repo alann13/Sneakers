eb9f34a4ebb9ef26878f9096a258ed81b81a0b7c
alann13 <53200557+alann13@users.noreply.github.com>
2024-02-27 22:53:41 -0500

    added some functionality

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,54 @@
 import './main.css'
+
+window.addEventListener('DOMContentLoaded', () => {
+  const ui = {
+    imageList: document.querySelectorAll<HTMLImageElement>(
+      '#product-image-list li > img'
+    ),
+    carouselImage: document.querySelector<HTMLImageElement>(
+      '.active-carousel-img'
+    ),
+    addBtn: document.querySelector<HTMLButtonElement>('.add-btn'),
+    minusBtn: document.querySelector<HTMLButtonElement>('.minus-btn'),
+    addToCartNum: document.querySelector<HTMLParagraphElement>(
+      '.add-to-cart-number'
+    ),
+    addToCartBtn: document.querySelector('.add-to-cart-btn'),
+    cartIcon: document.querySelector('.cart-icon-btn'),
+    avatarBtn: document.querySelector('.avatar-btn'),
+    cartDialog: document.querySelector<HTMLDialogElement>('.cart-dialog'),
+  }
+
+  for (const image of ui.imageList) {
+    image.addEventListener('click', (event) => {
+      const imgNumb =
+        event.currentTarget?.attributes['data-image-product'].value
+      ui.carouselImage!.src = `/image-product-${imgNumb}.jpg`
+    })
+  }
+
+  ui.addBtn?.addEventListener('click', () => {
+    let num = Number(ui.addToCartNum?.textContent) + 1
+    ui.addToCartNum!.textContent = num.toString()
+  })
+
+  ui.minusBtn?.addEventListener('click', () => {
+    if (ui.addToCartNum?.textContent >= '1') {
+      let num = Number(ui.addToCartNum?.textContent) - 1
+      ui.addToCartNum!.textContent = num.toString()
+    }
+  })
+
+  ui.addToCartBtn?.addEventListener('click', () => {
+    console.log('added to cart')
+  })
+
+  ui.cartIcon?.addEventListener('click', () => {
+    console.log('cart icon here')
+    ui.cartDialog!.showModal()
+  })
+
+  ui.avatarBtn?.addEventListener('click', () => {
+    console.log('avatar btn here here')
+  })
+})
